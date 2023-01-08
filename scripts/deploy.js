8115f9e322be0462b89d5325b598eda2d8c271c0
OmarMWarraich <earthling986@gmail.com>
2023-01-08 18:24:26 +0500

    chore: Initialize Hardhat

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy();
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
